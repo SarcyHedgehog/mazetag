bae22d8c75722d6b11cedca40d56db777ce26292
SarcyHedgehog <jpoole@sarcastichedgehog.com>
2025-06-16 22:12:11 +0100

    Initial commit of Maze Tag 3D game

---
+++ config.example.js
@@ -0,0 +1,6 @@
+// config.example.js - Commit this file
+// Instructions: Copy this file to config.js and fill in your actual values.
+window.APP_CONFIG = {
+  API_KEY: "Your API Key from multisynq.io/code",
+  APP_ID: "com.domain.game  (Reversed domain name and game name)",
+};
